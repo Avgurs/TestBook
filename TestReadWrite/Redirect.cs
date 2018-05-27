ca5ef498b7f64cd07e3821d9473fc5557db77513
Avgurs <seregepochta@gmail.com>
2018-05-27 16:15:51 +0300

    Redirect

---
+++ TestReadWrite/Redirect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+
+namespace TestReadWrite
+{
+    class Redirect
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter log_out = null;
+            try
+            {
+                log_out = new StreamWriter("logfile.txt");
+                Console.SetOut(log_out);
+
+                Console.WriteLine("Начало файла");
+
+                for (int count = 0; count < 10; count++)
+                    Console.WriteLine("Count {0}", count);
+
+                Console.WriteLine("End of file");
+
+            }
+            catch (IOException exc)
+            {
+                Console.WriteLine("Ошибка ввода-вывода файла\n" + exc.Message);
+            }
+            finally
+            {
+                if (log_out != null) log_out.Close();
+            }
+        }
+    }
+}
