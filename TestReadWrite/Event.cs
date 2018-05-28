356f4dcc58a57ee2a1c81d80b99167ba21218cbb
winpersh <winpersh@gmail.com>
2018-05-28 16:58:25 +0300

    Event

---
+++ TestReadWrite/Event.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TestDelegate
+{
+    delegate void MyEventHandler();
+    class MyEvent
+    {
+        public event MyEventHandler SomeEvent;
+
+        public void OnSomeEvent()
+        {
+            if (SomeEvent != null)
+                SomeEvent();
+        }
+    }
+    class EventDemo
+    {
+        static void Handler()
+        {
+            Console.WriteLine("Произошло событие");
+        }
+        static void Main(string[] args)
+        {
+            MyEvent evt = new MyEvent();
+
+            evt.SomeEvent += Handler;
+
+            evt.OnSomeEvent();
+            Console.ReadKey();
+
+        }
+
+    }
+}
