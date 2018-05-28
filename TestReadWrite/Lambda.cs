df22d0dc24a7b06a4cdbd790f989847f0be82159
winpersh <winpersh@gmail.com>
2018-05-28 16:15:51 +0300

    Lambda

---
+++ TestReadWrite/Lambda.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TestDelegate
+{
+    delegate string StrMod(string str);
+    class UseStatementLambdas
+    {
+        //Тест Lambda
+
+        static void Main(string[] args)
+        {
+            StrMod ReplaceSpaces = NewMethod();
+
+            StrMod RemoveSpaces = s =>
+            {
+                Console.WriteLine("Удаление пробелов.");
+                string temp = "";
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] != ' ') temp += s[i];
+                }
+                return temp;
+            };
+
+            StrMod strOp = RemoveSpaces;
+            string str;
+
+            str = strOp("Это простой тест.");
+            Console.WriteLine(str);
+            Console.WriteLine();
+
+            strOp = ReplaceSpaces;
+            str = strOp("Это простой тест.");
+            Console.WriteLine(str);
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+
+        private static StrMod NewMethod()
+        {
+            return s =>
+            {
+                Console.WriteLine("Замена пробелов дефисами.");
+                return s.Replace(' ', '-');
+            };
+        }
+    }
+}
