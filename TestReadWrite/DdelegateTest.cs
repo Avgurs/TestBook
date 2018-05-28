4cc11643e196722ee98efe2b56442db5a3b39f1a
Avgurs <seregepochta@gmail.com>
2018-05-28 10:30:50 +0300

    Delegate

---
+++ TestReadWrite/DdelegateTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+
+namespace TestDelegate
+{
+    delegate string StrMod(string str);
+    class DelegateTest
+    {
+        static string ReplaceSpaces(string s)
+        {
+            Console.WriteLine("Замена пробелов дефисами.");
+            return s.Replace(' ', '-');
+        }
+
+        static string RemoveSpaces(string s)
+        {
+            Console.WriteLine("Удаление пробелов.");
+            string temp = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ') temp += s[i];
+            }
+            return temp;
+        }
+
+        static void Main(string[] args)
+        {
+            StrMod strOp = new StrMod(ReplaceSpaces);
+            string str;
+
+            str = strOp("Это простой тест.");
+            Console.WriteLine(str);
+            Console.WriteLine();
+
+            strOp = new StrMod(RemoveSpaces);
+            str = strOp("Это простой тест.");
+            Console.WriteLine(str);
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
